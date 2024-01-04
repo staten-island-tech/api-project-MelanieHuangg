83161bad294a6697486d93e54019e988e5d064c1
MelanieHuangg <melanie.huang0306@gmail.com>
2024-01-04 02:26:17 -0500

    api

--- vite-project/js/api.js
+++ vite-project/js/api.js
@@ -1,1 +1,15 @@
-async function GetData()
+const URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/20"
+
+async function getData(URL) {
+
+    try {
+        const response =  await fetch(URL);
+        const data = await response.json();
+
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getData(URL)
